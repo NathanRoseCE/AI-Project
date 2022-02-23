adfdd58016337bed3ca3be67e42f7a44fdcbff68
Nathan <rosen4@my.erau.edu>
2022-02-23 11:04:28 -0500

    added responsibiliteies

--- Documentation/Proposal/auto/main.el
+++ Documentation/Proposal/auto/main.el
@@ -3,9 +3,15 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("biblatex" "backend=biber" "style=ieee")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
-    "art11"))
+    "art11"
+    "biblatex"
+    "hyperref")
+   (LaTeX-add-bibliographies
+    "resources"))
  :latex)
 
